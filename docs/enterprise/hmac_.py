cc6a503e39542b5d58fb9487de1da27f8c0ef782
François-Guillaume Ribreau <github@fgribreau.com>
2020-04-13 13:27:35 +0200

    feat: add support for URL encoding in python example

--- docs/enterprise/hmac_.py
+++ docs/enterprise/hmac_.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from urllib.parse import urlencode
+from urllib.parse import urlencode, quote_plus
 import hmac, hashlib, codecs
 
 def sign(query, secretKey):
@@ -11,18 +11,22 @@ if __name__ == '__main__':
   ACCOUNT_ID = 'MY_ACCOUNT_ID'
   SECRET_KEY = 'MY_SECRET_KEY'
 
+  # Do not activate character encoding if you do not need parameter values
+  # containing URL unsafe characters like "&".
+  ENCODE_VALUES = True
+
   # Then generate the watermark-free url
   # no need to encode the query string, Image-Charts will decode every parameters by itself to check the signature
   # learn why in our documentation https://documentation.image-charts.com/enterprise/
   rawQuerystring = [
       ('cht', 'bvs'),
       ('chd', 's:93zyvneTTO'),
-      ('chtt', 'Hello world'),
+      ('chtt', 'Hello world 20%'),
       ('chs', '400x401'),
       ('icac', ACCOUNT_ID)  # don't forget to add your account id before signing it
   ]
 
-  queryString = "&".join( [ param +'='+ value for (param, value) in rawQuerystring ] )
+  queryString = "&".join( [ param +'='+ (quote_plus(value) if ENCODE_VALUES else value) for (param, value) in rawQuerystring ] )
   signature = sign(queryString, SECRET_KEY)
   publicUrl = "https://image-charts.com/chart?" + queryString + "&ichm=" + signature
 
